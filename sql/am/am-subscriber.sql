4ec959ce5aef8de049b5d21e5099e0410c4f3e27
tharikaGitHub <tharika.m92@gmail.com>
2018-02-09 14:35:48 +0530

    Refactored datasource configuration

--- sql/am/am-subscriber.sql
+++ sql/am/am-subscriber.sql
@@ -2,4 +2,4 @@ UPDATE AM_SUBSCRIBER
 SET USER_ID = `pseudonym`, CREATED_BY = `pseudonym`, EMAIL_ADDRESS = ''
 WHERE USER_ID = `username`
       AND CREATED_BY = `username`
-      AND TENANT_ID = `tenant_domain`
+      AND TENANT_ID = -1234
