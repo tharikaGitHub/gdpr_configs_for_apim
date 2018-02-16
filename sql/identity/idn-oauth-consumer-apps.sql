45b96fbff0767e5d2747b0dc3866aba01e323df3
tharikaGitHub <tharika.m92@gmail.com>
2018-02-16 13:22:47 +0530

    Added queries to files that supports all dbs

--- sql/identity/idn-oauth-consumer-apps.sql
+++ sql/identity/idn-oauth-consumer-apps.sql
@@ -1,5 +1,5 @@
-UPDATE IDN_OAUTH_CONSUMER_APPS
-SET USERNAME = `pseudonym`
-WHERE USERNAME = `username`
-      AND USER_DOMAIN = `user_store_domain`
+UPDATE IDN_OAUTH_CONSUMER_APPS 
+SET USERNAME = `pseudonym`, APP_NAME = REPLACE(APP_NAME, CONCAT(`username`, '_'), CONCAT(`pseudonym`, '_')) 
+WHERE USERNAME = `username` 
+      AND USER_DOMAIN = `user_store_domain` 
       AND TENANT_ID = `tenant_id`
--- sql/identity/sp-app.sql
+++ sql/identity/sp-app.sql
@@ -1,5 +1,5 @@
-UPDATE SP_APP
-SET USERNAME = `pseudonym`
-WHERE USERNAME = `username`
-      AND USER_STORE = `user_store_domain`
+UPDATE SP_APP 
+SET USERNAME = `pseudonym`, APP_NAME = REPLACE(APP_NAME, CONCAT(`username`, '_'), CONCAT(`pseudonym`, '_')), DESCRIPTION = REPLACE(DESCRIPTION, CONCAT(CONCAT(' ', `username`), '_'), CONCAT(CONCAT(' ', `pseudonym`), '_')) 
+WHERE USERNAME = `username` 
+      AND USER_STORE = `user_store_domain` 
       AND TENANT_ID = `tenant_id`
